23f139df41d9f9f8f0e98ab4cc9c84489ec8d74e
sebastian <sebastian.cerdam@usach.cl>
2018-05-09 22:14:20 -0300

    update front

--- src/main/resources/static/js/controllers/BL-master-controller.js
+++ src/main/resources/static/js/controllers/BL-master-controller.js
@@ -1,14 +1,48 @@
-app.controller('BLController', ['$scope', '$location', function($scope, $location) {
+app.controller('BLController', ['$scope', '$location', '$http', function($scope, $location, $http) {
     $scope.nombre = 'Nacho';
     $scope.tipoBL = ['Exportación', 'Importación'];
     $scope.numeroOperacion = 1452;
     $scope.creador = 'Eduardo Avendaño';
     $scope.fecha = new Date();
-
-
+    //$scope.ciudades;
+   // var list = ciuda.map(x => x.id);
 
 
     $scope.isActive = function(route) {
         return route === $location.path();
     }
+    $scope.newBLMaster= {
+        blocked:false,
+        servicio:'maritimo2jhgg',
+        n_reserva:12345,
+        nViaje:'55',
+        agenteCreador:'ALUSA',
+        tipoNegocio:'algo',
+        destino:'STGO',
+        naviera:1,
+        container:1
+
+    };
+
+    $scope.send = function(){
+        $http.post("http://localhost:8080/blmasters",$scope.newBLMaster);
+        console.log($scope.newBLMaster);
+
+        //aca newVotation esta listo para ser utilizado en el método POST, en teoría
+
+    }
+
+    /*$scope.ciudades = function() {
+        $http.get('http://localhost:8080/ciudades').then(function (response) {
+            $scope.ciudades = response.data;});
+            //console.log($scope.ciudades[0]);
+    }*/
+
+
+    $http.get('http://localhost:8080/ciudades').then(function(response){
+        $scope.ciudades = response.data;
+        console.log($scope.ciudades);
+    })
+    console.log($scope.ciudades);
+
 }]);
