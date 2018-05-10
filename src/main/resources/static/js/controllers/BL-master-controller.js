c3c8b3dc460dee51c261f7c257d7241a6151dfaf
sebastian <sebastian.cerdam@usach.cl>
2018-05-09 22:58:56 -0300

    update front: obtener campos desde el front 2

--- src/main/resources/static/js/controllers/BL-master-controller.js
+++ src/main/resources/static/js/controllers/BL-master-controller.js
@@ -39,10 +39,32 @@ app.controller('BLController', ['$scope', '$location', '$http', function($scope,
     }*/
 
 
-    $http.get('http://localhost:8080/ciudades').then(function(response){
+    $http.get('http://localhost:8080/ciudades').then(function(response){  // campo: destino
         $scope.ciudades = response.data;
         console.log($scope.ciudades);
     })
-    console.log($scope.ciudades);
+
+    $http.get('http://localhost:8080/navieras').then(function(response){
+        $scope.navieras = response.data;
+        //console.log($scope.navieras);
+    })
+
+    $http.get('http://localhost:8080/naves').then(function(response){
+        $scope.naves = response.data;
+        //console.log($scope.naves);
+    })
+
+    $http.get('http://localhost:8080/puertos').then(function(response){   //campos: pto origen y descarga
+        $scope.puertos = response.data;
+        //console.log($scope.puertos);
+    })
+
+    $http.get('http://localhost:8080/empresas').then(function(response){ // campo: agente
+        $scope.empresas = response.data;
+        //console.log($scope.puertos);
+    })
+
+
+
 
 }]);
