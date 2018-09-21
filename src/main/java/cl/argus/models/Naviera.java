bb514004f0bbee322fd467388ea79eb4868cf8a3
GabrielNacho <gabriel.venegas@usach.cl>
2018-09-21 10:16:18 -0300

    fix naves/naviera

--- src/main/java/cl/argus/models/Naviera.java
+++ src/main/java/cl/argus/models/Naviera.java
@@ -13,9 +13,7 @@ public class Naviera {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
     private String nombre;
-    @JsonIgnore
-    @OneToMany(mappedBy = "naviera",fetch = FetchType.EAGER)
-    private Set<BLMaster> blMasters;
+
 
     public long getId(){
         return this.id;
